0c4b851923e54a5a9b5f26ab03b60e084e9790de
dkim0756 <danielyk9@gmail.com>
2018-09-22 03:55:40 -0400

    Rewards

--- src/Pages/Rewards.js
+++ src/Pages/Rewards.js
@@ -1,79 +1,184 @@
-import React, { Component } from 'react';
-import { Button, CardGroup } from 'semantic-ui-react';
-import * as firebase from 'firebase';
-import { Header, Icon, Image, Menu, Segment, Sidebar, Card } from 'semantic-ui-react'
+  import 'rc-progress/assets/index.css';
+  import React, { Component } from 'react';
+  import { Line, Circle } from 'rc-progress';
+  import { Button, CardGroup } from 'semantic-ui-react';
+  import * as firebase from 'firebase';
+  import { Header, Icon, Image, Menu, Segment, Sidebar, Card } from 'semantic-ui-react'
 
-class Rewards extends Component {
-  constructor(props) {
-    super(props);
+  class ProgressWheel extends Component {
+    constructor() {
+      super();
+      this.state = {
+        percent: 30,
+        color: '#3FC7FA',
+      };
+      this.changeState = this.changeState.bind(this);
+    }
 
-    // Add constructor
-  }
+    changeState() {
+      const colorMap = ['#3FC7FA', '#85D262', '#FE8C6A'];
+      const value = parseInt(Math.random() * 100, 10);
+      this.setState({
+        percent: value,
+        color: colorMap[parseInt(Math.random() * 3, 10)],
+      });
+    }
 
-  render() {
-    return (
-      <div>
-      <div>
-        <Card.Group>
-          <Card raised onClick={() => { }}>
-            <Card.Content centered>
-              <Image centered src='./goodwillGiftcard.png' size='medium' />
-              <Card.Description textAlign='center' style = {{marginTop: '5px'}}>
-                Goodwill Giftcard
-              </Card.Description>
-            </Card.Content>
-          </Card>
-          <Card raised onClick={() => { }}>
-            <Card.Content centered>
-              <Image centered src='./visaGiftcard.jpg' size='medium' />
-              <Card.Description textAlign='center' style = {{marginTop: '5px'}}>
-                Visa Giftcard
-              </Card.Description>
-            </Card.Content>
-          </Card>
-          <Card raised onClick={() => { }}>
-            <Card.Content centered>
-              <Image centered src='./paypalGiftcard.jpg' size='medium' />
-              <Card.Description textAlign='center' style = {{marginTop: '5px'}}>
-                Paypal Giftcard
-              </Card.Description>
-            </Card.Content>
-          </Card>
-          <Card raised onClick={() => { }}>
-            <Card.Content centered>
-              <Image centered src='./amazonGiftcard.jpg' size='medium' />
-              <Card.Description textAlign='center'>
-                Amazon Giftcard
-              </Card.Description>
-            </Card.Content>
-          </Card>
-          <Card raised onClick={() => { }}>
-            <Card.Content centered>
-              <Image centered src='./ebayGiftcard.jpg' size='medium' />
-              <Card.Description textAlign='center' style = {{marginTop: '5px'}}>
-                Ebay Giftcard
-              </Card.Description>
-            </Card.Content>
-          </Card>
-          <Card raised onClick={() => { }}>
-            <Card.Content centered>
-              <Image centered src='./walmartGiftcard.jpg' size='medium' />
-              <Card.Description textAlign='center' style = {{marginTop: '5px'}}>
-                Walmart Giftcard
-              </Card.Description>
-            </Card.Content>
-          </Card>
-        </Card.Group>
+    render() {
+      const containerStyle = {
+        width: '250px',
+      };
+      const circleContainerStyle = {
+        width: '250px',
+        height: '250px',
+        display: 'inline-block',
+      };
+      return (
+        <div>
+          <h3>Progress towards next reward: {40}%</h3>
+          <div style={circleContainerStyle}>
+            <Circle
+              percent={this.state.percent}
+              strokeWidth="6"
+              strokeLinecap="round"
+              strokeColor={this.state.color}
+            />
+          </div>
+          <div>
+        <div>
+          <Card.Group>
+            <Card raised onClick={() => { }}>
+              <Card.Content centered>
+                <Image centered src='./goodwillGiftcard.png' size='medium' />
+                <Card.Description textAlign='center' style = {{marginTop: '5px'}}>
+                  Goodwill Giftcard: 100 pts
+                </Card.Description>
+              </Card.Content>
+            </Card>
+            <Card raised onClick={() => { }}>
+              <Card.Content centered>
+                <Image centered src='./visaGiftcard.jpg' size='medium' />
+                <Card.Description textAlign='center' style = {{marginTop: '5px'}}>
+                  Visa Giftcard: 200 pts
+                </Card.Description>
+              </Card.Content>
+            </Card>
+            <Card raised onClick={() => { }}>
+              <Card.Content centered>
+                <Image centered src='./paypalGiftcard.jpg' size='medium' />
+                <Card.Description textAlign='center' style = {{marginTop: '5px'}}>
+                  Paypal Giftcard: 200 pts
+                </Card.Description>
+              </Card.Content>
+            </Card>
+            <Card raised onClick={() => { }}>
+              <Card.Content centered>
+                <Image centered src='./amazonGiftcard.jpg' size='medium' />
+                <Card.Description textAlign='center'>
+                  Amazon Giftcard: 200 pts
+                </Card.Description>
+              </Card.Content>
+            </Card>
+            <Card raised onClick={() => { }}>
+              <Card.Content centered>
+                <Image centered src='./ebayGiftcard.jpg' size='medium' />
+                <Card.Description textAlign='center' style = {{marginTop: '5px'}}>
+                  Ebay Giftcard: 200 pts
+                </Card.Description>
+              </Card.Content>
+            </Card>
+            <Card raised onClick={() => { }}>
+              <Card.Content centered>
+                <Image centered src='./walmartGiftcard.jpg' size='medium' />
+                <Card.Description textAlign='center' style = {{marginTop: '5px'}}>
+                  Walmart Giftcard: 200 pts
+                </Card.Description>
+              </Card.Content>
+            </Card>
+          </Card.Group>
+        </div>
+        
       </div>
-      <div class="ui progress">
-      <div class="bar">
-        <div class="progress"></div>
-      </div>
-      <div class="label">Rewards Progress</div>
-    </div>
-    </div>
-    )
+        </div>
+      
+    );
   }
 }
 
-export default Rewards;
+export default ProgressWheel;
+
+
+// class Rewards extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     // Add constructor
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//       <div>
+//         <Card.Group>
+//           <Card raised onClick={() => { }}>
+//             <Card.Content centered>
+//               <Image centered src='./goodwillGiftcard.png' size='medium' />
+//               <Card.Description textAlign='center' style = {{marginTop: '5px'}}>
+//                 Goodwill Giftcard
+//               </Card.Description>
+//             </Card.Content>
+//           </Card>
+//           <Card raised onClick={() => { }}>
+//             <Card.Content centered>
+//               <Image centered src='./visaGiftcard.jpg' size='medium' />
+//               <Card.Description textAlign='center' style = {{marginTop: '5px'}}>
+//                 Visa Giftcard
+//               </Card.Description>
+//             </Card.Content>
+//           </Card>
+//           <Card raised onClick={() => { }}>
+//             <Card.Content centered>
+//               <Image centered src='./paypalGiftcard.jpg' size='medium' />
+//               <Card.Description textAlign='center' style = {{marginTop: '5px'}}>
+//                 Paypal Giftcard
+//               </Card.Description>
+//             </Card.Content>
+//           </Card>
+//           <Card raised onClick={() => { }}>
+//             <Card.Content centered>
+//               <Image centered src='./amazonGiftcard.jpg' size='medium' />
+//               <Card.Description textAlign='center'>
+//                 Amazon Giftcard
+//               </Card.Description>
+//             </Card.Content>
+//           </Card>
+//           <Card raised onClick={() => { }}>
+//             <Card.Content centered>
+//               <Image centered src='./ebayGiftcard.jpg' size='medium' />
+//               <Card.Description textAlign='center' style = {{marginTop: '5px'}}>
+//                 Ebay Giftcard
+//               </Card.Description>
+//             </Card.Content>
+//           </Card>
+//           <Card raised onClick={() => { }}>
+//             <Card.Content centered>
+//               <Image centered src='./walmartGiftcard.jpg' size='medium' />
+//               <Card.Description textAlign='center' style = {{marginTop: '5px'}}>
+//                 Walmart Giftcard
+//               </Card.Description>
+//             </Card.Content>
+//           </Card>
+//         </Card.Group>
+//       </div>
+//       <div class="ui progress">
+//       <div class="bar">
+//         <div class="progress"></div>
+//       </div>
+//       <div class="label">Rewards Progress</div>
+//     </div>
+//     </div>
+//     )
+//   }
+// }
+
+// export default Rewards;
