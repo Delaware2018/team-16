8ae4bcc6f0a2a0b28a395cb2f6ee7c1c826c7dad
Max <gomlms@gmail.com>
2018-09-22 03:04:26 -0400

    Added Donation Features and Firebase Integration

--- src/Components/Donation/DonationMeter.js
+++ src/Components/Donation/DonationMeter.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Button } from 'semantic-ui-react';
 import * as firebase from 'firebase';
 import { Header, Input, Image, Menu, Segment, Sidebar, Card } from 'semantic-ui-react'
+import Donation from '../../Data/Donation'
 
 class DonationMeter extends Component {
   constructor(props) {
@@ -12,8 +13,27 @@ class DonationMeter extends Component {
 
   }
 
-  donate = () => {
-    console.log("WOW")
+  donate = (e) => {
+    var user = this.props.globalState.user;
+
+    var donation = new Donation(this.props.globalState.username, this.state.donation);
+    user.donations.push(donation);
+
+    var userJSON = JSON.stringify(user);
+
+    var storageRef = firebase.storage().ref().child(this.props.globalState.username + '.json');
+    var userBlob = new Blob([userJSON], {type: 'application/json'});
+
+    storageRef.put(userBlob).then((snapshot) => {
+      console.log("Uploaded User with donation");
+    })
+
+  }
+
+  setDonation = (e) => {
+    this.setState({
+      donation: e.currentTarget.value
+    })
   }
 
   render() {
@@ -23,6 +43,7 @@ class DonationMeter extends Component {
           action={<Button onClick={this.donate}>Donate</Button>}
           actionPosition='left'
           placeholder='Donate to Goodwill'
+          onChange={this.setDonation}
           defaultValue=''
         />
       </div>
