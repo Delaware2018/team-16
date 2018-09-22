3f40480467bcea2143baa571708bf8bd8d6c7ce2
Stoney Fentonmiller <stoneyfentonmiller@yahoo.com>
2018-09-22 03:31:49 -0400

    new toggle lgog

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -99,7 +99,7 @@ class Main extends Component {
         {this.state.loggedIn && <div>
           
         </div>}
-        <Button onClick={ this.buttonAway } id="one">Toggle Menu</Button>
+        <Button onClick={ this.buttonAway } icon='angle double right' id="one" />
         <NavBar logout={this.logout} goToPage={this.goToPage} showMenu={this.state.showMenu} globalState={this.props.globalState} />
 
         {this.state.currentPage === 'Home' &&
